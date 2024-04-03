da64e3814434c4cbdc66b95a7bbbfd32350efcfd
Nazgul1985 <145559608+Nazgul1985@users.noreply.github.com>
2024-04-03 21:49:23 +0500

    Django 37-40

---
+++ Create_table.sql
@@ -0,0 +1,6 @@
+Create Table book
+(Id SERIAL PRIMARY KEY,
+ title CHARACTER VARYING (60),
+ author CHARACTER VARYING (60),
+ pyblished_year INTEGER
+)
---
+++ insert_table.sql
@@ -0,0 +1,4 @@
+Insert Into book (title, author, pyblished_year)
+Values ('Nazgul','Koshkinona', 2024),
+	('Ascar', 'Zakenov', 2023),
+	('Azamat', 'Alichanov', 2024);
